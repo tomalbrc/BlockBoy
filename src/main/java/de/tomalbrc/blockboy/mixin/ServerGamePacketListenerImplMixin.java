d5be0e76368c5c35220dc9d1667957ad1751b3ca
tomalbrc <tomalbrc@me.com>
2024-04-21 18:28:58 +0200

    backport 1.20.4 changes...

--- src/main/java/de/tomalbrc/blockboy/mixin/ServerGamePacketListenerImplMixin.java
+++ src/main/java/de/tomalbrc/blockboy/mixin/ServerGamePacketListenerImplMixin.java
@@ -2,17 +2,12 @@ package de.tomalbrc.blockboy.mixin;
 
 import de.tomalbrc.blockboy.gui.MapGui;
 import eu.pb4.sgui.virtual.VirtualScreenHandlerInterface;
-import net.minecraft.network.Connection;
 import net.minecraft.network.chat.LastSeenMessages;
-import net.minecraft.network.protocol.game.ServerboundChatCommandPacket;
-import net.minecraft.network.protocol.game.ServerboundCommandSuggestionPacket;
-import net.minecraft.network.protocol.game.ServerboundPlayerCommandPacket;
-import net.minecraft.network.protocol.game.ServerboundPlayerInputPacket;
+import net.minecraft.network.protocol.game.*;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.level.ServerPlayer;
-import net.minecraft.server.network.CommonListenerCookie;
-import net.minecraft.server.network.ServerCommonPacketListenerImpl;
 import net.minecraft.server.network.ServerGamePacketListenerImpl;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -20,13 +15,13 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ServerGamePacketListenerImpl.class)
-public abstract class ServerGamePacketListenerImplMixin extends ServerCommonPacketListenerImpl {
+public abstract class ServerGamePacketListenerImplMixin {
     @Shadow
     public ServerPlayer player;
 
-    public ServerGamePacketListenerImplMixin(MinecraftServer minecraftServer, Connection connection, CommonListenerCookie commonListenerCookie) {
-        super(minecraftServer, connection, commonListenerCookie);
-    }
+    @Shadow
+    @Final
+    private MinecraftServer server;
 
     @Inject(method = "handleCustomCommandSuggestions", at = @At("HEAD"), cancellable = true)
     private void blockboy$handleCustomCommandSuggestions(ServerboundCommandSuggestionPacket packet, CallbackInfo ci) {
