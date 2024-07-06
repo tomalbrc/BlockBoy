57f07e5369eef46de62077852ba2da4bdbf7e354
tomalbrc <tomalbrc@me.com>
2024-07-06 15:56:07 +0200

    0.5

--- src/main/java/de/tomalbrc/blockboy/mixin/ServerGamePacketListenerImplMixin.java
+++ src/main/java/de/tomalbrc/blockboy/mixin/ServerGamePacketListenerImplMixin.java
@@ -1,14 +1,19 @@
 package de.tomalbrc.blockboy.mixin;
 
+import de.tomalbrc.blockboy.BlockBoy;
 import de.tomalbrc.blockboy.gui.MapGui;
+import eu.pb4.sgui.api.gui.HotbarGui;
 import eu.pb4.sgui.virtual.VirtualScreenHandlerInterface;
 import net.minecraft.network.chat.LastSeenMessages;
 import net.minecraft.network.protocol.game.ServerboundChatCommandSignedPacket;
 import net.minecraft.network.protocol.game.ServerboundCommandSuggestionPacket;
 import net.minecraft.network.protocol.game.ServerboundPlayerCommandPacket;
 import net.minecraft.network.protocol.game.ServerboundPlayerInputPacket;
+import net.minecraft.network.protocol.game.*;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.server.network.ServerGamePacketListenerImpl;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.phys.Vec3;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -65,6 +70,31 @@ public abstract class ServerGamePacketListenerImplMixin {
             this.player.server.execute(() -> {
                 mapGui.executeCommand(string);
             });
+            ci.cancel();
+        }
+    }
+
+    @Inject(method = "handleInteract", at = @At("HEAD"), cancellable = true)
+    private void blockboy$onInteract(ServerboundInteractPacket serverboundInteractPacket, CallbackInfo ci) {
+        if (BlockBoy.activeSessions.containsKey(this.player)) {
+            serverboundInteractPacket.dispatch(new ServerboundInteractPacket.Handler() {
+                @Override
+                public void onInteraction(InteractionHand interactionHand) {
+                    BlockBoy.activeSessions.get(player).onClickEntity(0, HotbarGui.EntityInteraction.INTERACT, false, Vec3.ZERO);
+                }
+
+                @Override
+                public void onInteraction(InteractionHand interactionHand, Vec3 vec3) {
+                    BlockBoy.activeSessions.get(player).onClickEntity(0, HotbarGui.EntityInteraction.INTERACT, false, Vec3.ZERO);
+
+                }
+
+                @Override
+                public void onAttack() {
+                    BlockBoy.activeSessions.get(player).onClickEntity(0, HotbarGui.EntityInteraction.ATTACK, false, Vec3.ZERO);
+                }
+            });
+
             ci.cancel();
         }
     }
