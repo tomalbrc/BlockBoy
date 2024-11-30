f099309374fe251c49fc3318a1796ac66e3fc917
tomalbrc <tomalbrc@me.com>
2024-11-30 18:07:42 +0100

    hopefully fix disconnect issue

--- src/main/java/de/tomalbrc/blockboy/BlockBoy.java
+++ src/main/java/de/tomalbrc/blockboy/BlockBoy.java
@@ -4,9 +4,11 @@ import de.tomalbrc.blockboy.gui.EmulatorGui;
 import it.unimi.dsi.fastutil.objects.Reference2ObjectArrayMap;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
+import net.fabricmc.fabric.api.networking.v1.ServerPlayConnectionEvents;
 import net.minecraft.world.entity.player.Player;
 
 import java.util.Map;
+import java.util.Objects;
 
 public class BlockBoy implements ModInitializer {
     public static Map<Player, EmulatorGui> activeSessions = new Reference2ObjectArrayMap<>();
@@ -14,5 +16,12 @@ public class BlockBoy implements ModInitializer {
     @Override
     public void onInitialize() {
         CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) -> BlockBoyCommand.register(dispatcher));
+        ServerPlayConnectionEvents.DISCONNECT.register((serverGamePacketListener, server) -> {
+            if (BlockBoy.activeSessions.containsKey(serverGamePacketListener.player)) {
+                Objects.requireNonNull(BlockBoy.activeSessions.get(serverGamePacketListener.player).getController()).stopEmulation();
+                Objects.requireNonNull(BlockBoy.activeSessions.get(serverGamePacketListener.player)).close();
+                BlockBoy.activeSessions.remove(serverGamePacketListener.player);
+            }
+        });
     }
 }
